54ad3ef7068a036f08783794151f3dad6cf48534
YosmanProgramer <Yosman.higuita028@gmail.com>
2021-11-04 16:44:09 -0500

    fix

--- assets/js/default.js
+++ assets/js/default.js
@@ -4,17 +4,11 @@
 const navMobile = document.querySelector('.nav-mobile');
 const buttonMenu = document.querySelector('.menu-button');
 const iconMenu = document.querySelector('.menu-icon');
-const buttonDownload = document.querySelector('.button-download');
-const buttonDownloadLink = document.querySelector('.button-download--link');
 
 // EVENTS HANDLERS
-buttonMenu.addEventListener('click', function(e) {
+buttonMenu.addEventListener('click', function (e) {
     // SHOW MENU BAR
     navMobile.classList.toggle('nav-mobile--active');
     // SHOW ICON TRANSITION
     iconMenu.classList.toggle('menu-icon--active');
-})
-
-buttonDownload.addEventListener('click', function(e) {
-    buttonDownloadLink.classList.toggle('hidden');
 })
